02721eb331df837b85a6df9565433c9efc99166c
Riley Flynn <riley@rileyflynn.me>
2017-04-17 15:35:25 -0230

    Added solarized dark scheme, and changed method of swapping stylesheets to prevent a white flash in between

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 function changeCSS(cssFile) {
-
     var oldlink = document.getElementsByTagName("link").item(0);
-
-    var newlink = document.createElement("link");
-    newlink.setAttribute("rel", "stylesheet");
-    newlink.setAttribute("type", "text/css");
-    newlink.setAttribute("href", "styles/" + cssFile);
-
-    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+    oldlink.setAttribute("href", "styles/" + cssFile);
 }
