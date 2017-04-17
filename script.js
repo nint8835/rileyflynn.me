dfc9de776b669c04bbb19da67797b6a049960429
Riley Flynn <riley@rileyflynn.me>
2017-04-17 15:12:24 -0230

    Implemented basics for stylesheet changing

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeCSS(cssFile) {
+
+    var oldlink = document.getElementsByTagName("link").item(0);
+
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+}
