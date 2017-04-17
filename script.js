be6df2aa5dc9b619bea7acd352e7f4b2a656a188
Riley Flynn <riley@rileyflynn.me>
2017-04-17 15:24:46 -0230

    Began adding switching controls, and moved colour schemes to their own folder

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function changeCSS(cssFile) {
     var newlink = document.createElement("link");
     newlink.setAttribute("rel", "stylesheet");
     newlink.setAttribute("type", "text/css");
-    newlink.setAttribute("href", cssFile);
+    newlink.setAttribute("href", "styles/" + cssFile);
 
     document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
 }
