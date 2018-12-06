b10af80751712b2489ed95e3c041d352628d7d63
Riley Flynn <riley@rileyflynn.me>
2018-12-06 14:53:59 -0330

    Removed old site

--- old/script.js
+++
@@ -1,8 +0,0 @@
-function changeColour(cssFile) {
-    var oldlink = document.getElementsByTagName("link").item(1);
-    oldlink.setAttribute("href", "styles/colour_schemes/" + cssFile + ".css");
-}
-function changeFont(cssFile) {
-    var oldlink = document.getElementsByTagName("link").item(2);
-    oldlink.setAttribute("href", "styles/fonts/" + cssFile + ".css");
-}
