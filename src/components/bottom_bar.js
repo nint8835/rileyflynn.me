d81181b06dc8038d9008a69c702dfa6d0b630ac6
Riley Flynn <riley@rileyflynn.me>
2019-07-21 16:42:52 -0230

    Added credits to bottom bar

--- src/components/bottom_bar.js
+++ src/components/bottom_bar.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 import bottomBarStyles from "./styles/bottom_bar.module.css";
+import { Link } from "gatsby";
 
 export default () => (
   <div className={bottomBarStyles.bottomBar}>
-    <div className={bottomBarStyles.leftItems}>I'm on the left!</div>
-    <div className={bottomBarStyles.rightItems}>I'm on the right!</div>
+    <div className={bottomBarStyles.leftItems}></div>
+    <div className={bottomBarStyles.rightItems}>
+      <Link to="/credits" key="/credits" className={bottomBarStyles.link}>
+        Credits
+      </Link>
+    </div>
   </div>
 );
