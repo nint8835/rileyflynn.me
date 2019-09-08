767a535c0039e2dfd4089c51a63817ed9fbe0942
Riley Flynn <riley@rileyflynn.me>
2019-09-07 22:55:56 -0230

    Added carbon thumbnails

--- src/components/head.js
+++ src/components/head.js
@@ -1,24 +1,22 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-import st_johns from "../../static/images/st_johns.jpg";
-
 export default ({ title, description, path }) => (
   <Helmet>
     <title>{title} - rileyflynn.me</title>
     <meta name="description" content={description} />
-    <meta name="image" content={st_johns} />
+    <meta name="image" content={`screenshots/${title}.png`} />
 
     <meta property="og:type" content="website" />
     <meta property="og:url" content={"https://rileyflynn.me" + path} />
     <meta property="og:title" content={title + " - rileyflynn.me"} />
     <meta property="og:description" content={description} />
-    <meta property="og:image" content={st_johns} />
+    <meta property="og:image" content={`screenshots/${title}.png`} />
 
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:creator" content="@BootlegJohn" />
     <meta name="twitter:title" content={title + " - rileyflynn.me"} />
     <meta name="twitter:description" content={description} />
-    <meta name="twitter:image" content={st_johns} />
+    <meta name="twitter:image" content={`screenshots/${title}.png`} />
   </Helmet>
 );
