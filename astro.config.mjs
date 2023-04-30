c45b3f3d1946f3ad8312e11a11bd1b39b85fe406
Riley Flynn <riley@rileyflynn.me>
2023-04-30 20:22:08 -0230

    Correct target URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
+    site: process.env.CF_PAGES_URL || 'http://localhost:3000',
 });
