cdb53ef0ef7bccf9294ad7811cbff62a350686f4
Riley Flynn <riley@rileyflynn.me>
2024-01-07 12:04:37 -0330

    Add meta tags dev tool

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import metaTags from 'astro-meta-tags';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [tailwind(), metaTags()],
     site: process.env.CF_PAGES_URL || 'http://localhost:3000',
 });
