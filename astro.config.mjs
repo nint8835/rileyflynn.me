a790d0dcb1850b025c9f4dec94985e9f2a095edf
Riley Flynn <riley@rileyflynn.me>
2024-03-26 18:55:56 -0230

    Re-add meta tag addon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
+import metaTags from 'astro-meta-tags';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [tailwind(), metaTags()],
 });
