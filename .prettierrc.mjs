8e15ec9c9d1ea1982afecdd681b38429393f0029
Riley Flynn <riley@rileyflynn.me>
2024-03-25 21:46:57 -0230

    Reformat, re-add tailwind prettier plugin

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,17 +1,17 @@
 /** @type {import("prettier").Config} */
 export default {
-  semi: true,
-  trailingComma: "all",
-  singleQuote: true,
-  printWidth: 120,
-  tabWidth: 4,
-  plugins: ["prettier-plugin-astro"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 4,
+    plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+    overrides: [
+        {
+            files: '*.astro',
+            options: {
+                parser: 'astro',
+            },
+        },
+    ],
 };
