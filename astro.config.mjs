1e041f2208ec3b056e6c0d53a9fd4f813414c6dd
Riley Flynn <riley@rileyflynn.me>
2024-03-25 21:40:10 -0230

    Nuke and start over

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
 
-import metaTags from 'astro-meta-tags';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), metaTags()],
-    site: process.env.CF_PAGES_URL || 'http://localhost:3000',
+  integrations: [tailwind()],
 });
--- src/utils/date_formats.ts
+++
@@ -1,4 +0,0 @@
-export const MonthYearDateFormat = new Intl.DateTimeFormat('en-US', {
-    month: 'long',
-    year: 'numeric',
-});
