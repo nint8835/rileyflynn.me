47d34847f78f9d5213c675db612e0801b3f0f095
Riley Flynn <riley@rileyflynn.me>
2024-04-15 20:29:52 -0230

    Simple footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import tailwind from '@astrojs/tailwind';
+import metaTags from 'astro-meta-tags';
 import { defineConfig } from 'astro/config';
 
-import metaTags from 'astro-meta-tags';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), metaTags()],
+    integrations: [tailwind(), metaTags(), icon()],
     site: process.env.CF_PAGES_URL || 'http://localhost:4321',
     trailingSlash: 'always',
 });
