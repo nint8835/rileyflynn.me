7fa68570b7f0d010d546a4e993d78c03e680b5f5
Riley Flynn <riley@rileyflynn.me>
2024-08-25 15:07:26 -0230

    Get rid of most of the existing layout
    This rewrite is going to turn into a Stackcoin 2020 situation, isn't it

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import tailwind from '@astrojs/tailwind';
 import metaTags from 'astro-meta-tags';
 import { defineConfig } from 'astro/config';
 
-import icon from 'astro-icon';
-
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), metaTags(), icon()],
+    integrations: [tailwind(), metaTags()],
     site: process.env.CF_PAGES_URL || 'http://localhost:4321',
     trailingSlash: 'always',
 });
