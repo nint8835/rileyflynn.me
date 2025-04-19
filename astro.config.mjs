334f34374cf0f0581477ad769b6b594b80171cc3
Riley Flynn <riley@rileyflynn.me>
2025-04-19 13:27:15 -0230

    Fallback to monospace

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ export default defineConfig({
                 provider: fontProviders.fontsource(),
                 name: 'Iosevka',
                 cssVariable: '--font-iosevka',
+                fallbacks: ['monospace'],
             },
         ],
     },
