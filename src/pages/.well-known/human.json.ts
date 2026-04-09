6a72bda1ab320399ab38507aeed3cec5a2169e47
Riley Flynn <riley@rileyflynn.me>
2026-04-09 17:18:18 +0200

    Add friends to human.json

--- src/pages/.well-known/human.json.ts
+++ src/pages/.well-known/human.json.ts
@@ -1,16 +1,32 @@
 type Vouch = { url: string; vouched_at: string };
 type HumanJson = { url: string; version: '0.1.1'; vouches: Vouch[] };
 
+const VOUCHED_URLS: string[] = [
+    // My other sites
+    'https://bootleg.technology',
+    // Friends' sites
+    'https://blog.pirated.tech',
+    'https://danielledonnelly.github.io/marketing-portfolio',
+    'https://danielpower.ca',
+    'https://ecrann.dev',
+    'https://ethandenny.dev',
+    'https://evaan.dev',
+    'https://jackharrhy.dev',
+    'https://mitchellhynes.com',
+    'https://moustafaelsisy.com',
+];
+
 export function GET({}) {
+    const vouchTime = new Date();
+    const vouchTimeString = vouchTime.toISOString().split('T')[0];
+
     const humanJson: HumanJson = {
         url: (import.meta.env.SITE + import.meta.env.BASE_URL).replace(/\/$/, ''),
         version: '0.1.1',
-        vouches: [
-            {
-                url: 'https://bootleg.technology',
-                vouched_at: '2026-04-09',
-            },
-        ],
+        vouches: VOUCHED_URLS.map((url) => ({
+            url,
+            vouched_at: vouchTimeString,
+        })),
     };
 
     return new Response(JSON.stringify(humanJson), {
