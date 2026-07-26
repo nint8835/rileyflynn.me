aac1f16b1e9aed662c9302239c05d4bcbf9b61d7
Riley Flynn <riley@rileyflynn.me>
2026-07-26 19:19:10 -0230

    Fix OG image URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import tailwindcss from '@tailwindcss/vite';
 import metaTags from 'astro-meta-tags';
 import { defineConfig, fontProviders } from 'astro/config';
 
+// Support Cloudflare dev URLs as well as the proper domain for prod
+const isProd = process.env.CF_PAGES_BRANCH && process.env.CF_PAGES_BRANCH === 'main';
+const url = isProd ? 'https://rileyflynn.me' : process.env.CF_PAGES_URL;
+
 export default defineConfig({
     integrations: [metaTags(), mdx()],
-    site: process.env.CF_PAGES_URL || 'http://localhost:3000',
+    site: url || 'http://localhost:4321',
     vite: {
         plugins: [tailwindcss()],
     },
